1bf9867078ae3acfe3e36fa85bd478075f0fe908
ReZero <ghostofpast@icloud.com>
2023-04-26 16:08:49 +0300

    add task001

---
+++ task001_N_to_1Rec/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+int n = Readint("Введите натуральное число: ");
+int x = 1;
+
+if (n > 0)
+{
+    Console.WriteLine(IntervalOfNumbers(n, x));
+}
+else
+{
+    Console.WriteLine("Не корректный ввод!");
+}
+
+int IntervalOfNumbers(int n, int x)
+{
+    if (n == x) return n;
+    else 
+    {
+        Console.Write($"{IntervalOfNumbers(n, x + 1)}, ");
+    }
+    return x;
+}
+
+int Readint(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
