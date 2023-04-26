6fc611c9e8b2d4800c3f0c0da759b2cf4303746c
ReZero <ghostofpast@icloud.com>
2023-04-26 16:09:04 +0300

    add task002

---
+++ task002_N_to_MRec/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int n = Readint("Введите большее натуральное число: ");
+int m = Readint("Введите меньшее натуральное число: ");
+
+if (n > 0 || m > 0)
+{
+    Console.WriteLine(SummOfNumbers(n, m));
+}
+else
+{
+    Console.WriteLine("Не корректный ввод!");
+}
+
+int SummOfNumbers(int n, int m)
+{
+    if (n == m) return n;
+    else 
+    {
+        return m + SummOfNumbers(n, m + 1);
+    }
+}
+
+int Readint(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
